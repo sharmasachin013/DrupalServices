f283d836bbeec7aaafa8968627aeaeb173252ec5
Sachin Sharma <sharma.sachin013@gmail.com>
2025-09-22 06:11:28 +0000

    theme code updated

---
+++ web/themes/ctimestheme/js/ctimestheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * ctimestheme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ctimestheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
